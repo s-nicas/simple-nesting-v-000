642aeebca18bdef546c45774e50c61c2ed2bacb4
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-26 00:31:25 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -132,5 +132,5 @@ def adding_to_dennis
         }
      }
 
-  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  programmer_hash[:dennis_ritchie][:languages][1].push ("Assembly")
 end
